25a327898d653ad49872a80c083e2022bda74a6b
AMin2000025 <mouhamedamine.rachid@etudiant-enit.utm.tn>
2026-04-11 13:29:52 +0100

    modified Nom.java

--- Nom.java
+++ Nom.java
@@ -1,3 +1,18 @@
 public class Nom {
+    private String nomComplet;
+    private int id;
+
+    public Nom(String nomComplet, int id) {
+        this.nomComplet = nomComplet;
+        this.id = id;
+    }
+    public String getNomComplet() {
+        return nomComplet;
+    }
+
+    public int getId() {
+        return id;
+    }
     
+
 }
