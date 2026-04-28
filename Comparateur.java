0d6d1c35ae916860ce5dde17038aaf45e37f647a
damikarim41@gmail.com <damikarim41@gmail.com>
2026-04-29 00:10:20 +0100

    Update comparateurs and add test class

--- Comparateur.java
+++ Comparateur.java
@@ -1,33 +1,4 @@
-public class Comparateur {
-    private Nom nom1 ;
-    private Nom nom2 ;
-    private int score ;
+public interface Comparateur {
 
-    public int comparer(Nom nom1, Nom nom2){
-        //algorithme de comparaison ....
-        int s =0;
-        
-        return s ;
-
-
-
-    }
-    public Comparateur(Nom nom1,Nom nom2){
-        this.nom1=nom1;
-        this.nom2=nom2;
-        this.score=comparer(nom1,nom2);
-
-    } 
-    public Comparateur(Nom nom1,Nom nom2, int s){
-        this.nom1=nom1;
-        this.nom2=nom2;
-        this.score=s;
-    }
-
-    
-    public int getScore (Nom nom1,Nom nom2){
-        
-        return this.score ;
-    }
-
+    public double comparer(Nom nom1, Nom nom2);
 }
--- Nom.java
+++ Nom.java
@@ -1,19 +1,19 @@
-public abstract class Personne {
-    protected String nomComplet;
-    protected String id;
+public class Nom {
+    private String nomOriginal;
+    private String nomTraite;
 
-    public Personne(String nomComplet, String id) {
-        this.nomComplet = nomComplet;
-        this.id = id;
+    public Nom(String nomOriginal, String nomTraite) {
+        this.nomOriginal = nomOriginal;
+        this.nomTraite = nomTraite;
 
     }
 
-    public String getNomComplet() {
-        return nomComplet;
+    public String getNomOriginal() {
+        return nomOriginal;
     }
 
-    public String getId() {
-        return id;
+    public String getNomTraite() {
+        return nomTraite;
     }
 
 }
