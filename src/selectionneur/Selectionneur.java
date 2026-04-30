c4cf7637060aee7c1c8ea5946bcf7bbc824d6f9d
AMin2000025 <mouhamedamine.rachid@etudiant-enit.utm.tn>
2026-04-30 10:41:19 +0100

    Update Selectionneur.java

--- src/selectionneur/Selectionneur.java
+++ src/selectionneur/Selectionneur.java
@@ -1,16 +1,7 @@
 public class Selectionneur {
     
     
-    private Nom[] nomSelectionnes ;
-    private int scoreMinimal;
-    private int nbrMaximal;
-    public Selectionneur(int s,int n){
-        this.nbrMaximal=n;
-        this.scoreMinimal=s;
-    }
-    public void selectionner (){
-        
-    }
+   public Triplet[] selectionner (Triplet[]){}
 
     
 }
