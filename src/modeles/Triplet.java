bc18518445c470728d88aac93d46dbab51189de3
AMin2000025 <mouhamedamine.rachid@etudiant-enit.utm.tn>
2026-04-30 10:35:02 +0100

    Update and rename Listes.java to Triplet.java

---
+++ src/modeles/Triplet.java
@@ -0,0 +1,10 @@
+package modeles;
+
+public class Triplet {
+    private Nom nom1;
+    private Nom nom2;
+    private Double score;
+
+}
+// plus de modification....
+// cette class peut etre supprimée ou fusionnée avec une autre class
