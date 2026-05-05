e51821d9b194f14c362254fda1ee133a1aea5b77
damikarim41@gmail.com <damikarim41@gmail.com>
2026-05-05 23:03:41 +0100

    jfbqlvb

--- src/Candidats.java
+++ src/Candidats.java
@@ -1,5 +1,17 @@
 public class Candidats {
-    private Nom nom1;
-    private Nom nom2;
+    private final Nom nom1;
+    private final Nom nom2;
 
+    public Candidats(Nom nom1, Nom nom2) {
+        this.nom1 = nom1;
+        this.nom2 = nom2;
+    }
+
+    public Nom getNom1() {
+        return nom1;
+    }
+
+    public Nom getNom2() {
+        return nom2;
+    }
 }
--- src/Nom.java
+++ src/Nom.java
@@ -1,24 +1,35 @@
+import java.util.List;
+
+
 public class Nom {
     private String nomOriginal;
-    private String nomTraite;
+    private List<String> nomTraite;
     private String id;
 
-    public Nom(String nomOriginal, String nomTraite, String id) {
+    public Nom(String nomOriginal, String id) {
         this.nomOriginal = nomOriginal;
-        this.nomTraite = nomTraite;
         this.id = id;
+    }
+
+    public Nom(List<String> nomTraite) {
+        super();
+        this.nomTraite = nomTraite;
     }
 
     public String getNomOriginal() {
         return nomOriginal;
     }
 
-    public String getNomTraite() {
+    public String getId() {
+        return id;
+    }
+
+    public List<String> getNomTraite() {
         return nomTraite;
     }
 
-    public String getId() {
-        return id;
+    public void setNomTraite(List<String> nomTraite) {
+        this.nomTraite = nomTraite;
     }
 
 }
