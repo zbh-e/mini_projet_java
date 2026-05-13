a564621ed7f500be81f4700e8c7873ac893f04f6
zbh <>
2026-05-13 22:27:40 +0100

    interface livreur

--- src/Livreur.java
+++ src/Livreur.java
@@ -1,28 +1,4 @@
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.List;
 
-public class Livreur {
+public interface Livreur {
+     void livrer(List<Triplet> livreson);}
 
-    public void livrer(List<Triplet> triplets, String fichier) {
-        try (PrintWriter writer = new PrintWriter(new FileWriter(fichier))) {
-            // En-tête CSV
-            writer.println("nom1,nom2,score");
-
-            // Lignes de données
-            for (Triplet t : triplets) {
-                writer.printf("%s,%s,%.4f%n",
-                    t.getNom1().getNomOriginal(),
-                    t.getNom2().getNomOriginal(),
-                    t.getScore()
-                );
-            }
-
-            System.out.println("Fichier sauvegardé : " + fichier);
-
-        } catch (IOException e) {
-            System.err.println("Erreur lors de l'écriture : " + e.getMessage());
-        }
-    }
-}
